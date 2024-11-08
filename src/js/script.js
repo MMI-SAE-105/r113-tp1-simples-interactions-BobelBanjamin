96742097d55b1423009eb4b0548d95291846f7ce
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-11-08 12:28:58 +0300

    Réagir a un click

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,11 @@
 Amettreenrouge = document.querySelector("#a-mettre-en-rouge")
 
 Amettreenrouge.style.color = 'red'
+
+// Réagir a un click
+
+EnRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+
+EnRougeSuiteAClick.addEventListener("click", (evt)=>{
+    EnRougeSuiteAClick.style.color = "red"
+})
