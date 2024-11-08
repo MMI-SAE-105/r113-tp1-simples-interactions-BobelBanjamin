1930938c347f0f32bcac218b060a69a097804aea
BobelBanjamin <banjamin.bobel@edu.univ-fcomte.fr>
2024-11-08 12:37:34 +0300

    trouver tout les h2 plus ajout const

--- src/js/script.js
+++ src/js/script.js
@@ -2,14 +2,23 @@
 
 // Changer la couleur du texte
 
-Amettreenrouge = document.querySelector("#a-mettre-en-rouge")
+const Amettreenrouge = document.querySelector("#a-mettre-en-rouge")
 
-Amettreenrouge.style.color = 'red'
+ Amettreenrouge.style.color = 'red'
 
 // Réagir a un click
 
-EnRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
+const EnRougeSuiteAClick = document.querySelector("#en-rouge-suite-a-click")
 
-EnRougeSuiteAClick.addEventListener("click", (evt)=>{
+ EnRougeSuiteAClick.addEventListener("click", (evt)=>{
     EnRougeSuiteAClick.style.color = "red"
 })
+
+// Trouver tous les H2
+
+const collH2 = document.querySelectorAll("H2");
+ collH2.forEach((elm)=>{
+    elm.addEventListener("click", (evt)=>{
+        evt.target.style.color ="red";
+    });
+});
